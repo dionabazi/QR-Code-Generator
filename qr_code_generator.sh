b3e018606ed07da73699ef0e6e3d501a3ee6b292
Tech4ever <142183299+dionabazi@users.noreply.github.com>
2024-10-15 21:09:19 +0000

    Update qr_code_generator.sh

--- qr_code_generator.sh
+++ qr_code_generator.sh
@@ -1,8 +1,22 @@
 #!/bin/bash
 
-# Prompt user for input
+animate_figlet() {
+    local text="$1"
+    local figlet_output
+    figlet_output=$(echo "$text" | figlet)
+    IFS=$'\n' read -rd '' -a lines <<< "$figlet_output"
+    for line in "${lines[@]}"; do
+        for (( i=0; i<${#line}; i++ )); do
+            echo -ne "${line:$i:1}"
+            sleep 0.01
+        done
+        echo
+    done
+}
+
+animate_figlet "By dionabazi"
+
 echo "Enter the data for the QR code (URL, text, etc.):"
 read data
 
-# Run the Python script to generate the QR code
 python3 qr_code_generator.py "$data"
